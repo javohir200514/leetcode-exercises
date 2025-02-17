61a5f7406455bd97fa440b5ba1fd821ad81a1c2e
Javohir <komilbayevj@gmail.com>
2025-02-18 01:13:25 +0500

    add files

--- src/easy/math/powerOfThree/Main.java
+++ src/easy/math/powerOfThree/Main.java
@@ -1,11 +1,9 @@
 package easy.math.powerOfThree;
 
-import easy.math.powerOfFour.Solution5;
-
 public class Main {
     public static void main(String[] args) {
 
-        Solution5 solution=new Solution5();
+        Solution6 solution=new Solution6();
         int n=16;
         System.out.print(solution.isPowerOfThree(n));
     }
