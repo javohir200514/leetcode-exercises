fda410f26fd9b76f0a6c611d8028dd0f09864428
Javohir <komilbayevj@gmail.com>
2025-02-19 23:24:01 +0500

    add files

---
+++ src/easy/math/mySqrt/Main.java
@@ -0,0 +1,9 @@
+package easy.math.mySqrt;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution10 solution=new Solution10();
+        int a=8;
+        System.out.println(solution.mySqrt(a));
+    }
+}
---
+++ src/easy/math/mySqrt/Solution10.java
@@ -0,0 +1,9 @@
+package easy.math.mySqrt;
+
+public class Solution10 {
+    public int mySqrt(int x) {
+        int result;
+        result=(int)Math.sqrt(x);
+        return result;
+    }
+}
