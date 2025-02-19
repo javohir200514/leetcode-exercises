4038b47fe8c1f1c822808b004125d704b63f3946
Javohir <komilbayevj@gmail.com>
2025-02-20 00:36:32 +0500

    add files

---
+++ src/easy/math/fibonacci_number/Solution12.java
@@ -0,0 +1,16 @@
+package easy.math.fibonacci_number;
+
+public class Solution12 {
+    public static int fib(int n) {
+        if(n<=1){
+            return n;
+        }
+        int fib0=0,fib1=1,f=0;
+        for(int i=2;i<=n;i++){
+            f=fib0+fib1;
+            fib0=fib1;
+            fib1=f;
+        }
+        return f;
+    }
+}
