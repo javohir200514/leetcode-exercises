09a586b9436f80c6468361ed6c76a9d6f14cc366
Javohir <komilbayevj@gmail.com>
2025-02-19 23:48:14 +0500

    add files

---
+++ src/easy/math/checkPerfectNumber/Main.java
@@ -0,0 +1,10 @@
+package easy.math.checkPerfectNumber;
+
+import easy.math.isPolidrome.Solution7;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution11 solution=new Solution11();
+        System.out.println(solution.checkPerfectNumber(6));
+    }
+}
