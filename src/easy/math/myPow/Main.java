3aebc88630a933c2e76cfbf98b39f503b17df915
Javohir <komilbayevj@gmail.com>
2025-02-21 00:17:32 +0500

    add files

---
+++ src/easy/math/myPow/Main.java
@@ -0,0 +1,8 @@
+package easy.math.myPow;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution=new Solution();
+        System.out.println(solution.myPow(3.1,4));
+    }
+}
---
+++ src/easy/math/myPow/Solution.java
@@ -0,0 +1,10 @@
+package easy.math.myPow;
+
+public class Solution {
+    public double myPow(double x, int n) {
+        double result;
+        result=Math.pow(x,n);
+        return result;
+    }
+
+}
