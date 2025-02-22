614df33efba014cabbbd43f1ec6b23aa5ffecf82
Javohir <komilbayevj@gmail.com>
2025-02-23 01:54:04 +0500

    add files

---
+++ src/medium/divideTwoIntegers/Main.java
@@ -0,0 +1,8 @@
+package medium.divideTwoIntegers;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution=new Solution();
+        System.out.println(solution.divide(-8,3));
+    }
+}
