aab379391b77ec240d6cc8e7a77f7be82919c635
Javohir <komilbayevj@gmail.com>
2025-02-28 01:48:51 +0500

    add files

--- src/easy/math/toLowerCase_100/Main.java
+++ src/easy/math/toLowerCase_100/Main.java
@@ -1,4 +1,4 @@
-package easy.math.toLowerCase;
+package easy.math.toLowerCase_100;
 
 public class Main {
     public static void main(String[] args) {
--- src/easy/math/toLowerCase_100/Solution.java
+++ src/easy/math/toLowerCase_100/Solution.java
@@ -1,4 +1,4 @@
-package easy.math.toLowerCase;
+package easy.math.toLowerCase_100;
 
 public class Solution {
     public String toLowerCase(String s) {
